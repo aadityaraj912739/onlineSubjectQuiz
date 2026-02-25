29a84692dc72f73a3ec993e9919c31ca452f77fd
aadityaraj912739 <ar912739@gmail.com>
2026-02-26 00:08:55 +0530

    fix: Add Vercel URL to CORS policy

--- mcq-quiz/backend/server.js
+++ mcq-quiz/backend/server.js
@@ -18,7 +18,8 @@ app.use(express.json());
 app.use(cors({
     origin: [
         'http://localhost:3000',
-        'http://localhost:3001'
+        'http://localhost:3001',
+        'https://online-subject-quiz.vercel.app'
     ],
     credentials: true
 }));
